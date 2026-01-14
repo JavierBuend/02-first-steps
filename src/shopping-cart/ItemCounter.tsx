69993fff5e4d29aac5cfcfb4df45274cca5a3734
Javier <186595036+cepex1@users.noreply.github.com>
2026-01-14 14:16:54 +0100

    update final

--- src/shopping-cart/ItemCounter.tsx
+++ src/shopping-cart/ItemCounter.tsx
@@ -1,22 +1,41 @@
+import { useState } from "react";
+// import "./ItemCounter.css";
+import styles from "./ItemCounter.module.css";
+
 interface Props {
   productName: string;
   quantity: number | undefined;
 }
 
-export const ItemCounter = ({ productName, quantity }: Props) => {
+export const ItemCounter = ({ productName, quantity = 1 }: Props) => {
+  const [count, setCount] = useState(quantity);
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubstract = () => {
+    if (count === 1) {
+      return;
+    }
+    setCount(count - 1);
+  };
+
+  // const handleClick = () => {
+  //   console.log(`click ${productName}`);
+  // };
+
   return (
-    <section
-      style={{
-        display: "flex",
-        alignItems: "center",
-        gap: 10,
-        marginTop: 10,
-      }}
-    >
-      <span style={{ width: 100 }}>{productName}</span>
-      <button>+1</button>
-      <span>{quantity}</span>
-      <button>-1</button>
+    <section className={styles["item-row"]}>
+      <span
+        className={styles["item-text"]}
+        style={{ color: count === 1 ? "red" : "black" }}
+      >
+        {productName}
+      </span>
+      <button onClick={handleAdd}>+1</button>
+      <span>{count}</span>
+      <button onClick={handleSubstract}>-1</button>
     </section>
   );
 };
