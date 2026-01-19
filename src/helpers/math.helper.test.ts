bc4033c387f87273b51b27e1c0ccf05e2d54b27d
Javier <186595036+cepex1@users.noreply.github.com>
2026-01-19 11:52:33 +0100

    update 19/01

--- src/helpers/math.helper.test.ts
+++ src/helpers/math.helper.test.ts
@@ -1,14 +1,47 @@
-import { expect, test } from 'vitest'
-import { add } from './math.helper'
+import { describe, expect, test } from 'vitest';
+import { add, divide, multiply, substract } from './math.helper';
 
-test('should add two positive numbers', ()=>{
-
+describe('add', () => {
+  test('should add two positives numbers', () => {
     // ! 1. Arrange
     const a = 1;
-    const b = 5;
-    // ! 2. Act
-    const result = add(a,b);
-    // ! 3. Assert
+    const b = 2;
 
-    expect(result).toBe(a+b);
-})
+    // ! 2. Act
+    const result = add(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(a + b);
+  });
+
+  test('should add two negative numbers', () => {
+    // ! 1. Arrange
+    const a = -2;
+    const b = -4;
+
+    // ! 2. Act
+    const result = add(a, b);
+
+    // ! 3. Assert
+    expect(result).toBe(a + b);
+  });
+});
+
+describe('subtract', () => {
+  test('should subtract two positive numbers', () => {
+    const a = 2;
+    const b = 4;
+
+    const result = substract(a, b);
+
+    expect(result).toBe(a - b);
+  });
+  test('should subtract two negative numbers', () => {
+    const a = -2;
+    const b = -4;
+
+    const result = substract(a, b);
+
+    expect(result).toBe(a - b);
+  });
+});
